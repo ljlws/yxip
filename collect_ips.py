778d7fbe7b819acd9ed931e60208ce5b1de5bb23
Hmirror <55284070+ljlws@users.noreply.github.com>
2025-07-21 21:20:13 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -5,7 +5,8 @@ import os
 
 # 目标URL列表
 urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
-        'https://ip.164746.xyz'
+        'https://ip.164746.xyz',
+        'https://cf.090227.xyz/'
         ]
 
 # 正则表达式用于匹配IP地址
